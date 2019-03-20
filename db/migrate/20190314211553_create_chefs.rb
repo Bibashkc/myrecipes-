5947f0ba5dcb8f20282d493519630771e3c91b5e
bibash <bibashkc92@gmail.com>
2019-03-20 21:33:40 +0000

    changed chefnamer to chefname in Chef table

--- db/migrate/20190314211553_create_chefs.rb
+++ db/migrate/20190314211553_create_chefs.rb
@@ -1,7 +1,7 @@
 class CreateChefs < ActiveRecord::Migration[5.0]
   def change
     create_table :chefs do |t|
-      t.string :chefnamer
+      t.string :chefname
       t.string :email
       t.timestamps
     end
