09447a417d98aabaa1f7048bdf98f93dacc16e5b
bibash <bibashkc92@gmail.com>
2019-05-01 18:12:03 +0000

    added a redis url

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,1 @@
-$redis = Redis.new(url: ENV["REDIS_URL"]) if Rails.env.production?
+$redis = Redis.new(url: redis://h:p8c54d8d231510844340709002b5135fe6d6e8ce3a132172ac79e53cded749bac@ec2-3-212-99-166.compute-1.amazonaws.com:30729) if Rails.env.production?
